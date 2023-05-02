d5bfa18ee5e526b2b950f477588257c8a785810a
unknown <elias.braem@students.fhnw.ch>
2023-05-02 18:20:20 +0200

    basic chat finished

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,19 @@
 module Main exposing (..)
 
+
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
+import Html exposing (nav)
+import Html exposing (p)
+import Html exposing (ul)
+import Html exposing (li)
+import Html exposing (img)
+import Html.Attributes exposing (..)
+import Html exposing (input, Attribute)
+import Html.Events exposing (onInput)
+
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -28,8 +39,41 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+  div [ class "chat-container"] [
+    nav [][
+      ul [][
+        li [][div [class "user-icon"] []],
+        li [][div [class "chats-icon"] []],
+        li [][div [class "new-icon"] []]
+      ]
+    ],
+    div [class "chat-list"][
+      div [] [
+           text "Name 1"],
+      div [] [
+           text "Name 2"],
+      div [] [
+           text "Name 3"]
+
+    ],
+    div [ class "chat-wrapper"][
+      div [class "chat-header"] [
+           text "Name 1"],
+    div [class "chat"][
+      div [class "chat-item"] [
+           text "Nachricht 1"],
+      div [class "chat-item me"] [
+           text "Nachricht 2 dfgdg dfgsd gdg fsd"],
+      div [class "chat-item"] [
+           text "Nachricht 3 sdfsaf sdf "]
+    ],
+    div [class "chat-input"] [
+       input [ id "message",placeholder "Nachricht"] []
+    , button [class "primary-button"] [ text ("Senden") ]
     ]
+    ],
+    div [class "secure"] [
+      div [class "secure-icon"] [
+         ]
+    ]
+  ]
